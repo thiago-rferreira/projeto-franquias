1c2e2c5c8c10aecf8c0d60c344c0e613f791af18
thiago-rferreira <thiagoferreira435@gmail.com>
2025-09-30 19:43:13 -0300

    fix: p

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className={styles.main}>Hello World!
       <p>Teste</p>
       <p>Teste 2</p>
-      <p>Teste 3.1</p>
+      <p>Teste 3</p>
       <p>Teste Via Web</p>
     </div>
   )
