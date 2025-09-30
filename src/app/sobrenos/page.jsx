cb42b361efab420364df513902e4f3906e646d94
thiago-rferreira <thiagoferreira435@gmail.com>
2025-09-30 20:46:24 -0300

    feat: sobrenos

---
+++ src/app/sobrenos/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './sobrenos.module.css'
+
+
+function page() {
+  return (
+    <div className={styles.mainSobre}>Somos tops!</div>
+  )
+}
+
+export default page
