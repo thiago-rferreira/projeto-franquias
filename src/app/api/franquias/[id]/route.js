ded8ca7ca59859d12ae88632e2a75f26c2df5288
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-07 20:21:59 -0300

    comments for students

--- src/app/api/franquias/[id]/route.js
+++ src/app/api/franquias/[id]/route.js
@@ -1,14 +1,27 @@
+// Imports, sempre vai ser assim dentro de route.
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// GET, e precisa ter request, { params } 
 export async function GET(request, { params }) {
     try {
+        //Capturei o id pelo params.id, e só tive certeza que é INT
         const id = parseInt(params.id);
 
+        //Capturei franquia, no singular.
+        //Onde o id é o id que esta vindo de params
         const franquia = await prisma.franquia.findUnique({
-            where: { id }
+            // Where/Onde especifica quem
+            where: { id },
+            //Include pega o relacionamento e os funcionarios dele.
+            include: {
+                funcionarios: true,
+                _count: {
+                    select: { funcionarios: true }
+                }
+            }
         })
 
         if (!franquia) {
