54c95bf19cb01c33b97bdb65bf8f92cd02f1b3aa
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-10 20:35:17 -0300

    feat: getAll funcionarios

--- src/app/api/funcionarios/route.js
+++ src/app/api/funcionarios/route.js
@@ -1,3 +1,29 @@
-// Fazer  logica de funcionarios
-// Buscar do banco de dados
-// SQL Lite
+//Importar o nextResponse da lib next/server.
+import { NextResponse } from 'next/server'
+//Importar o client do prisma, pois ele que permite eu utilizar os comandos/codigos
+import { PrismaClient } from '@prisma/client'
+
+//Criar uma instancia/objeto/item do prisma
+const prisma = new PrismaClient()
+
+export async function GET() {
+    try {
+        const funcionarios = await prisma.funcionario.findMany({
+            include: {
+                franquia: true // Aqui eu incluo a franquia do funcionario na resposta
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        return NextResponse.json(funcionarios);
+
+    } catch (error) {
+        console.error('Erro ao buscar funcionários', error);
+        return NextResponse.json(
+            { error: 'Error interno de servidor!' },
+            { status: 500 }
+        )
+    }
+}
