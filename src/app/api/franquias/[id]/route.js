3603179786fafa54d1281aa1550ec4baf9747bf0
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-09 21:13:39 -0300

    feat: update franquias

--- src/app/api/franquias/[id]/route.js
+++ src/app/api/franquias/[id]/route.js
@@ -66,7 +66,6 @@ export async function DELETE(request, { params }) {
         }
 
         //Verificar se há dependencias com funcionarios
-
         if (franquia._count.funcionarios > 0) {
             return NextResponse.json(
                 { error: 'Não é possivel deletar franquia com funcionários ativos!' },
@@ -87,6 +86,64 @@ export async function DELETE(request, { params }) {
         console.error('Erro ao deletar franquia', error)
         return NextResponse.json(
             { error: 'Erro interno de servidor' },
+            { status: 500 }
+        )
+    }
+}
+
+export async function PUT(request, { params }) {
+    try {
+        //pegar o id
+        const id = parseInt(params.id)
+        //pega o dado novo para ser colocado no antigo
+        const data = await request.json()
+
+        const { nome, cidade, endereco, telefone } = data;
+
+        console.log(typeof (data))
+
+        // Ou Object.keys(data).length === 0
+        if (!data?.nome && !data?.cidade && !data?.endereco && !data?.telefone) {
+            return NextResponse.json(
+                { error: 'Voce precisa enviar algum dado' },
+                { status: 400 }
+            )
+        }
+
+        //Verificar se existe
+        const franquiaExiste = await prisma.franquia.findUnique({
+            where: { id }
+        })
+
+        if (!franquiaExiste) {
+            return NextResponse.json(
+                { error: 'Franquia não encontrada' },
+                { status: 404 }
+            )
+        }
+
+        // Se esta tudo OK, atualizo no banco de dados
+        const franquia = await prisma.franquia.update({
+            where: { id },
+            data: {
+                nome: nome ?? franquiaExiste.nome,
+                cidade: cidade ?? franquiaExiste.cidade,
+                endereco: endereco ?? franquiaExiste.endereco,
+                telefone: telefone ?? franquiaExiste.telefone
+            }
+        })
+
+        // Retorna a resposta
+        return NextResponse.json({
+            franquia: franquia,
+            msg: 'Franquia atualizada com sucesso!'
+        })
+
+
+    } catch (error) {
+        console.error('Erro ao atualizar franquia', error)
+        return NextResponse.json(
+            { error: 'Erro interno do servidor' },
             { status: 500 }
         )
     }
