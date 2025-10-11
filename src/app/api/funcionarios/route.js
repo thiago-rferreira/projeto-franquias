0b5b1e80076287b4c87a0f2d6c259a9c7baab47f
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-10 21:16:40 -0300

    feat: create funcionario

--- src/app/api/funcionarios/route.js
+++ src/app/api/funcionarios/route.js
@@ -27,3 +27,60 @@ export async function GET() {
         )
     }
 }
+
+export async function POST(request) {
+    try {
+        const data = await request.json()
+        const { nome, email, cargo, salario, franquiaId } = data;
+
+        //Validacao de todos os dados sao obrigatorios
+
+        if (!nome || !email || !cargo || !salario || !franquiaId) {
+            return NextResponse.json(
+                { error: 'Todos os campos são obrigatórios!' },
+                { status: 400 }
+            )
+        }
+
+        const franquiaExiste = await prisma.franquia.findUnique({
+            where: { id: parseInt(franquiaId) }
+        })
+
+        if (!franquiaExiste) {
+            return NextResponse.json(
+                { error: 'Id da franquia não existe, verifique.' },
+                { status: 404 }
+            )
+        }
+
+        const emailExiste = await prisma.funcionario.findUnique({
+            where: { email }
+        })
+
+        if (emailExiste) {
+            return NextResponse.json(
+                { error: 'Email já está em uso, tente outro!' },
+                { status: 400 }
+            )
+        }
+
+        const funcionario = await prisma.funcionario.create({
+            data: {
+                nome,
+                email,
+                cargo,
+                salario: parseFloat(salario),
+                franquiaId: parseInt(franquiaId)
+            }
+        })
+
+        return NextResponse.json(funcionario, { status: 201 })
+
+    } catch (error) {
+        console.error('Erro ao criar funcionário', error);
+        return NextResponse.json(
+            { error: 'Error interno de servidor!' },
+            { status: 500 }
+        )
+    }
+}
