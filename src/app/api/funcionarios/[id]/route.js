7ad49528829372841598d49fa70ca3453b4e1e2c
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-14 19:29:57 -0300

    feat: delete funcionarios api complete

--- src/app/api/funcionarios/[id]/route.js
+++ src/app/api/funcionarios/[id]/route.js
@@ -41,3 +41,43 @@ export async function GET(request, { params }) {
         )
     }
 }
+
+//Delete
+export async function DELETE(request, { params }) {
+    try {
+        //capturar o id pelo params
+        const id = parseInt(params.id);
+
+        // Verificar se existe funcionario
+        const existeFuncionario = await prisma.funcionario.findUnique({
+            where: { id }
+        })
+
+        // Verifico e dou uma mensagem de erro
+        if (!existeFuncionario) {
+            return NextResponse.json(
+                { error: 'Funcionário não encontrado' },
+                { status: 404 }
+            )
+        }
+
+        // Realiza o delete
+        await prisma.funcionario.delete({
+            where: { id }
+        })
+
+        // Resposta com o funcionario que foi apagado.
+        return NextResponse.json({
+            apagado: existeFuncionario,
+            msg: 'Funcionário apagada com sucesso'
+        })
+
+    } catch (error) {
+        console.error('Erro ao deletar funcionário', error)
+        return NextResponse.json(
+            { error: 'Erro interno de servidor' },
+            { status: 500 }
+        )
+    }
+
+}
