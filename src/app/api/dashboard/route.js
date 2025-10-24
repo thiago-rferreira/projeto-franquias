91deafd647f3cc1ea0da810c784f0dfd06c847c3
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-23 21:09:14 -0300

    feat: top5 franquias

--- src/app/api/dashboard/route.js
+++ src/app/api/dashboard/route.js
@@ -83,22 +83,89 @@ export async function GET() {
         // Ordenar do maior para o maior para o menor
         cidades.sort((a, b) => b.total - a.total)
 
-        /*
-         Como fazer pela query
+        //------------------- Agrupamento: Funcionarios por Cargo -------------------------
 
-        const cidades = await prisma.franquia.groupBy({
-        by: ['cidade'],
-        _count: { id: true },
-        orderBy: { _count: { id: 'desc' } }
-    })
+        const cargos = []
 
-       Resposta -> [{ cidade: 'São Paulo', _count: { id: 15 } }, ...]
+        funcionarios.forEach(funcionario => {
+            const existe = cargos.find(c => c.cargo === funcionario.cargo)
 
-        const resultado = cidades.map(c => ({
-        cidade: c.cidade,
-        total: c._count.id
-    }))
-        */
+            if (existe) {
+                existe.total++
+            } else {
+                cargos.push({
+                    cargo: funcionario.cargo,
+                    total: 1
+                })
+            }
+        })
+
+        cargos.sort((a, b) => b.total - a.total)
+
+
+        // ---------------- Agrupamento: faixa salario ------------------------
+        // Até 2000; De 2001 até 4000; De 4001 até 6000; De 6001 até 8000; Acima de 8000
+
+        //Contadores
+        let ate2mil = 0
+        let de2a4mil = 0
+        let de4a6mil = 0
+        let de6a8mil = 0
+        let acima8mil = 0
+
+        funcionarios.forEach(f => {
+            if (f.salario <= 2000) {
+                // ate2mil = ate2mil + 1
+                ate2mil++
+            } else if (f.salario <= 4000) {
+                de2a4mil++
+            } else if (f.salario <= 6000) {
+                de4a6mil++
+            } else if (f.salario <= 8000) {
+                de6a8mil++
+            } else {
+                acima8mil++
+            }
+        })
+
+        //Compor um array, para ficar bonito a info, e o front trabalhar mais facil
+
+        const faixasSalariais = [
+            { faixa: 'Até R$ 2000', quantidade: ate2mil },
+            { faixa: 'R$ 2001 até 4000', quantidade: de2a4mil },
+            { faixa: 'R$ 4001 até 6000', quantidade: de4a6mil },
+            { faixa: 'R$ 6001 até 8000', quantidade: de6a8mil },
+            { faixa: 'Acima de R$ 8000', quantidade: acima8mil }
+        ]
+
+
+        // ---------------- Top 5 Franquias, pela quantidade de funcionarios, exibindo tbm o salario total dessa franquia ---------------
+
+        const todasFranquias = []
+
+        franquias.forEach(franquia => {
+            //Calcular os salarios de todos os funcionarios dessa franquia
+
+            let salarios = 0
+            let quantidade = franquia.funcionarios.length
+
+            franquia.funcionarios.forEach(f => {
+                salarios += f.salario
+            })
+
+            todasFranquias.push({
+                id: franquia.id,
+                nome: franquia.nome,
+                cidade: franquia.cidade,
+                totalFuncionarios: quantidade,
+                totalSalario: salarios
+            })
+        })
+
+        //Ordenar todasFranquias
+        todasFranquias.sort((a, b) => b.totalFuncionarios - a.totalFuncionarios)
+
+
 
 
         // Retorno de tudo
@@ -108,7 +175,10 @@ export async function GET() {
             totalFuncionarios,
             somaSalarios,
             salarioMedio,
-            cidades
+            cidades,
+            cargos,
+            faixasSalariais,
+            todasFranquias
         }
 
         return NextResponse.json(dashboard)
